c81d40c2cc2bc20000cce4a3a4f2d02df4a1520a
roysonmenezes <royson32004@gmail.com>
2025-05-10 16:32:50 +0530

    added stockpge component

---
+++ Stock_Price_Aggregation/src/Stock_pages.jsx
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+import StockPage from './Stock_page';
+
+import { useNavigate } from 'react-router-dom';
+
+function App() {
+
+  
+
+  return (
+    <div className="App"> 
+    <StockPage />
+    </div>
+      
+        
+  );
+}
+
+export default App;
