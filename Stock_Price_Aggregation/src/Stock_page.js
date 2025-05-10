3225a2bdffeede394e47800b8a96ffef81eba290
roysonmenezes <royson32004@gmail.com>
2025-05-10 16:28:34 +0530

    formted stock pge

--- Stock_Price_Aggregation/src/Stock_page.js
+++ Stock_Price_Aggregation/src/Stock_page.js
@@ -19,23 +19,27 @@ function StockPage() {
 
 
   async function getData() {
-    console.log("Fetching data from API...");
+
+  console.log("Fetching data from API...");
   const url = "http://20.244.56.144/evaluation-service/stocks";
   try {
     const response = await fetch(url,
-        { method: "GET", headers: { "Content-Type": "application/json",
-             "Bearer": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ2ODY5MjE4LCJpYXQiOjE3NDY4Njg5MTgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6Ijk4Mzc4OWQ1LTI3NDQtNGQ4OS04YTkwLTU3ZWIwMjBmMjBjNyIsInN1YiI6IjIyazM4LnJveXNvbkBzamVjLmFjLmluIn0sImVtYWlsIjoiMjJrMzgucm95c29uQHNqZWMuYWMuaW4iLCJuYW1lIjoicm95c29uIGtlbnV0ZSBtZW5lemVzIiwicm9sbE5vIjoiNHNvMjJjYjAzOCIsImFjY2Vzc0NvZGUiOiJLakpBeFAiLCJjbGllbnRJRCI6Ijk4Mzc4OWQ1LTI3NDQtNGQ4OS04YTkwLTU3ZWIwMjBmMjBjNyIsImNsaWVudFNlY3JldCI6ImZmYVNQVHhuZXZWQ2ZQWkEifQ.B5GAEi8xAqFvHxptjCB89W_8UrAcaZ_P8EiSX9ct47E" } }
+        { 
+            method: "GET", 
+            headers: { "Content-Type": "application/json",
+             "Bearer": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQ2ODY5MjE4LCJpYXQiOjE3NDY4Njg5MTgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6Ijk4Mzc4OWQ1LTI3NDQtNGQ4OS04YTkwLTU3ZWIwMjBmMjBjNyIsInN1YiI6IjIyazM4LnJveXNvbkBzamVjLmFjLmluIn0sImVtYWlsIjoiMjJrMzgucm95c29uQHNqZWMuYWMuaW4iLCJuYW1lIjoicm95c29uIGtlbnV0ZSBtZW5lemVzIiwicm9sbE5vIjoiNHNvMjJjYjAzOCIsImFjY2Vzc0NvZGUiOiJLakpBeFAiLCJjbGllbnRJRCI6Ijk4Mzc4OWQ1LTI3NDQtNGQ4OS04YTkwLTU3ZWIwMjBmMjBjNyIsImNsaWVudFNlY3JldCI6ImZmYVNQVHhuZXZWQ2ZQWkEifQ.B5GAEi8xAqFvHxptjCB89W_8UrAcaZ_P8EiSX9ct47E" 
+            } 
+        }
     );
-    // if (!response.ok) {
-    //   throw new Error(`Response status: ${response.status}`);
-    // }
-
-    const json = await response.json();
-    console.log(json);
+   
+    const data = await response.json();
+    console.log(data);
   } catch (error) {
     console.error(error.message);
   }
 }
+
+
 
 // custom function to call the API
   function callApi( url ){
